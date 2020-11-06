bfbd87ef4f518e0e413a24ba6e0fd671955bb5b1
Will Mahler <35016013+WillMahler@users.noreply.github.com>
2020-11-05 20:00:34 -0500

    Implemented home page's RecyclerView. Closes #39, closes #20.
    Items are clickable and open the generic specific event page.
    Still needs more UI polish and handling for empty dataset - opened
      issues for those.
    Added a .gitignore file to exclude the .idea/ folder.
    Removed .idea/ folder from git cache, since we're using gradle, we
      shouldn't need it.

--- app/app/src/main/java/com/WKNS/gather/ui/home/HomeFragment.java
+++ app/app/src/main/java/com/WKNS/gather/ui/home/HomeFragment.java
@@ -5,35 +5,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.WKNS.gather.CreateEventActivity;
 import com.WKNS.gather.EventDetailsActivity;
 import com.WKNS.gather.R;
+import com.WKNS.gather.testData.Event;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private View root;
-    private Button button;
+    private HomeViewModel mHomeViewModel;
+    private RecyclerView mRecyclerView;
+    private HomeRecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private View mRoot;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        root = inflater.inflate(R.layout.fragment_home, container, false);
+        mHomeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        mRoot = inflater.inflate(R.layout.fragment_home, container, false);
 
-        button = root.findViewById(R.id.test);
-        button.setOnClickListener(new View.OnClickListener() {
+        mRecyclerView = mRoot.findViewById(R.id.recyclerView_Home);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(mRoot.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new HomeRecyclerViewAdapter(Event.testEvents());
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new HomeRecyclerViewAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View root) {
+            public void onItemClick(int position) {
+                /* TODO:
+                    This is where we would want to pass the Event at `position` in the dataset
+                    to the specific event page, so that it gets the info to display from
+                    the Event instance. This setup just opens it generically.
+                 */
                 Intent intent = new Intent(getActivity(), EventDetailsActivity.class);
                 startActivity(intent);
             }
         });
 
-        return root;
+        return mRoot;
     }
 }

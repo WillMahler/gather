6d34db7c4cafb58c6233eaf8ade5a3392d1af6cc
Will Mahler <mahler.will@gmail.com>
2020-11-07 00:00:50 -0500

    Implemented the RecyclerView for the Calendar page.  - Items are populated with the test Event data.  - RecyclerView is side-scrolling.  - Items are clickable and open the generic specific event page.  - Closes #52.

--- app/app/src/main/java/com/WKNS/gather/ui/calendar/CalendarFragment.java
+++ app/app/src/main/java/com/WKNS/gather/ui/calendar/CalendarFragment.java
@@ -5,25 +5,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.WKNS.gather.CreateEventActivity;
+import com.WKNS.gather.EventDetailsActivity;
 import com.WKNS.gather.R;
+
+import com.WKNS.gather.testData.Event;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class CalendarFragment extends Fragment {
 
-    private CalendarViewModel calendarViewModel;
-    private View root;
-    private FloatingActionButton fab;
+    private ArrayList<Event> mDataList;
+    private CalendarViewModel mCalendarViewModel;
+    private RecyclerView mRecyclerView;
+    private CalendarRecyclerViewAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private View mRoot;
+    private FloatingActionButton mFAB;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        calendarViewModel = ViewModelProviders.of(this).get(CalendarViewModel.class);
-        root = inflater.inflate(R.layout.fragment_calendar, container, false);
+        mCalendarViewModel = ViewModelProviders.of(this).get(CalendarViewModel.class);
+        mRoot = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-        fab = root.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mDataList = Event.testEvents();
+
+        mRecyclerView = mRoot.findViewById(R.id.recyclerView_Calendar);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(mRoot.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new CalendarRecyclerViewAdapter(mDataList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new CalendarRecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                /* TODO:
+                    This is where we would want to pass the Event at `position` in the dataset
+                    to the specific event page, so that it gets the info to display from
+                    the Event instance. This setup just opens it generically.
+                 */
+                Intent intent = new Intent(getActivity(), EventDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mFAB = mRoot.findViewById(R.id.fab);
+        mFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View root) {
                 Intent intent = new Intent(getActivity(), CreateEventActivity.class);
@@ -31,6 +69,6 @@ public class CalendarFragment extends Fragment {
             }
         });
 
-        return root;
+        return mRoot;
     }
 }

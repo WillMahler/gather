8a78f142a1bf4184702e9e9ad6ce24289ec8ed44
Shaun Moini <shaunmoinii@gmail.com>
2020-11-10 19:36:08 -0500

    Update app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
    Co-authored-by: Will Mahler <35016013+WillMahler@users.noreply.github.com>

--- app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
+++ app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 

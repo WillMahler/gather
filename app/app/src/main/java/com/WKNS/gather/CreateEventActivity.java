c32bebcc84b7ebfccd659e70397aa2da4fd08295
shaunmoini <shaunmoinii@gmail.com>
2020-11-12 22:30:14 -0500

    Create event overview tab completed. Closes #68.

--- app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
+++ app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
@@ -1,11 +1,21 @@
 package com.WKNS.gather;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.WKNS.gather.ui.tabbedViewFragments.CreateEventBudget;
 import com.WKNS.gather.ui.tabbedViewFragments.CreateEventSummary;
@@ -13,58 +23,94 @@ import com.WKNS.gather.ui.tabbedViewFragments.CreateEventTasks;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.Calendar;
+
 public class CreateEventActivity extends AppCompatActivity {
+
+    private Context context;
 
     private Toolbar actionbar;
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
     private SectionsPagerAdapter mAdapter;
 
-    private FloatingActionButton mFab;
+    private Fragment summaryFragment, taskFragment, budgetFragment;
+
+    private FloatingActionButton mFabCancel, mFabDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        context = this;
 
-        // Tab layout
-        mTabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        // View pager
-        mViewPager  = (ViewPager) findViewById(R.id.view_pager);
-
-        // Adapter
+        mTabLayout = findViewById(R.id.tabs);
+        mViewPager  = findViewById(R.id.view_pager);
         mAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // 2 Tabs
-        mAdapter.addFrag(new CreateEventSummary(), getString(R.string.tab_text_1));
-        mAdapter.addFrag(new CreateEventTasks(), getString(R.string.tab_text_2));
-        mAdapter.addFrag(new CreateEventBudget(), getString(R.string.tab_text_3));
+        summaryFragment = new CreateEventSummary();
+        taskFragment = new CreateEventTasks();
+        budgetFragment = new CreateEventBudget();
+
+        mAdapter.addFrag(summaryFragment, getString(R.string.tab_text_1));
+        mAdapter.addFrag(taskFragment, getString(R.string.tab_text_2));
+        mAdapter.addFrag(budgetFragment, getString(R.string.tab_text_3));
 
         mViewPager.setAdapter(mAdapter);
-
         mTabLayout.setupWithViewPager(mViewPager);
 
         // setting title of action bar
         actionbar = findViewById(R.id.actionbar);
         setSupportActionBar(actionbar);
         getSupportActionBar().setTitle("New Gathering");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_round_arrow_back_ios_24));
 
-        mFab = findViewById(R.id.fab2);
-        mFab.setOnClickListener(new View.OnClickListener() {
+        mFabCancel = findViewById(R.id.fab_cancel);
+        mFabDone = findViewById(R.id.fab_done);
+
+        mFabCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View root) {
-                finish();
+                final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Are you sure you want to abandon this event?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+        mFabDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Would you like to draft or publish this event?")
+                        .setCancelable(true)
+                        .setPositiveButton("Publish", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // publish event here
+                            }
+                        })
+                        .setNegativeButton("Draft", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // draft event here
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
--- app/app/src/main/java/com/WKNS/gather/ui/tabbedViewFragments/CreateEventSummary.java
+++ app/app/src/main/java/com/WKNS/gather/ui/tabbedViewFragments/CreateEventSummary.java
@@ -1,20 +1,86 @@
 package com.WKNS.gather.ui.tabbedViewFragments;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.WKNS.gather.CreateEventActivity;
 import com.WKNS.gather.R;
 
+import java.util.Calendar;
+
 public class CreateEventSummary extends Fragment {
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private EditText mTitle, mDate, mLocation, mDescription;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_create_event_summary, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mTitle = view.findViewById(R.id.editText_createEvent_title);
+        mDate = view.findViewById(R.id.editText_createEvent_date);
+        mLocation = view.findViewById(R.id.editText_createEvent_location);
+        mDescription = view.findViewById(R.id.editText_createEvent_description);
+
+        mDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    showDatePicker();
+                }
+            }
+        });
+
+        mDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month += 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mDate.setText(date);
+            }
+        };
+    }
+
+    private void showDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                getActivity(),
+                android.R.style.Theme_Holo_Light_Dialog,
+                mDateSetListener,
+                year, month, day
+        );
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    public void test(){
+
     }
 }

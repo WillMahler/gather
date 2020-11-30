e8f7248d1d946631fb7fb51dcd83aae2de27ffec
Shaun Moini <shaunmoinii@gmail.com>
2020-11-29 20:04:24 -0500

    - Pictures saved and retrieved for user profiles, pictures for events still in progress. - Calendar fragment removed and replaced with createEvent activity.

--- app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
+++ app/app/src/main/java/com/WKNS/gather/CreateEventActivity.java
@@ -1,62 +1,148 @@
 package com.WKNS.gather;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
-
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.WKNS.gather.ui.tabbedViewFragments.CreateEventSummary;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.WKNS.gather.databaseModels.Events.Event;
+import com.WKNS.gather.databaseModels.Users.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CreateEventActivity extends AppCompatActivity {
 
-    public static String TAG = CreateEventActivity.class.getSimpleName();
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private ArrayList<String> guestListArray;
     private Context context;
+    private FirebaseFirestore db;
+    private User userObject;
 
     private Toolbar actionbar;
-    private TabLayout mTabLayout;
-    private ViewPager mViewPager;
-    private SectionsPagerAdapter mAdapter;
-
-    private CreateEventSummary summaryFragment;
-
-    public FloatingActionButton mFabCancel, mFabDone;
+    private EditText mTitle, mDate, mLocation, mDescription, mGuestListView;
+    private Button mClearGuestList;
+    private FloatingActionButton mFabCancel, mFabDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
-        context = this;
-
-        mTabLayout = findViewById(R.id.tabs);
-        mViewPager  = findViewById(R.id.view_pager);
-        mAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        summaryFragment = new CreateEventSummary();
-
-        mAdapter.addFrag(summaryFragment, getString(R.string.tab_text_1));
-
-        mViewPager.setAdapter(mAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
 
         // setting title of action bar
         actionbar = findViewById(R.id.actionbar);
         setSupportActionBar(actionbar);
         getSupportActionBar().setTitle("New Gathering");
 
+        mTitle = findViewById(R.id.editText_createEvent_title);
+        mDate = findViewById(R.id.editText_createEvent_date);
+        mLocation = findViewById(R.id.editText_createEvent_location);
+        mDescription = findViewById(R.id.editText_createEvent_description);
+        mGuestListView = findViewById(R.id.editText_guest_list);
+        mClearGuestList = findViewById(R.id.clear_guests);
         mFabCancel = findViewById(R.id.fab_cancel);
         mFabDone = findViewById(R.id.fab_done);
+
+        guestListArray = new ArrayList<>();
+        context = this;
+        db = FirebaseFirestore.getInstance();
+
+        Gson gson = new Gson();
+        String userObjectString = getIntent().getStringExtra("userObjectString");
+        userObject = gson.fromJson(userObjectString, User.class);
+
+        mDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    showDatePicker();
+                }
+            }
+        });
+
+        mDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month += 1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mDate.setText(date);
+            }
+        };
+
+        mGuestListView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    showGuestDialog(v);
+                }
+            }
+        });
+
+        mGuestListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showGuestDialog(v);
+            }
+        });
+
+        mClearGuestList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(guestListArray.isEmpty()) {
+                    Toast.makeText(context, "Guest list is already empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Are you sure you want to clear the guest list?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                guestListArray.clear();
+                                mGuestListView.setText("");
+                                Toast.makeText(context, "Guest list cleared.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
 
         mFabCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +153,7 @@ public class CreateEventActivity extends AppCompatActivity {
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                summaryFragment.deleteEvent();
+                                deleteEvent();
                                 finish();
                             }
                         })
@@ -91,11 +177,11 @@ public class CreateEventActivity extends AppCompatActivity {
                         .setPositiveButton("Publish", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                if (summaryFragment.validateData()) {
+                                if (validateData()) {
                                     try {
-                                        summaryFragment.addEvent(true);
+                                        addEvent(true);
                                         Toast.makeText(context, "Event Published",
-                                                    Toast.LENGTH_SHORT).show();
+                                                Toast.LENGTH_SHORT).show();
                                     } catch (ParseException e) {
                                         e.printStackTrace();
                                         Toast.makeText(context, "Error: Event Not Published",
@@ -109,7 +195,7 @@ public class CreateEventActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 try {
-                                    summaryFragment.addEvent(false);
+                                    addEvent(false);
                                     Toast.makeText(context, "Draft Saved",
                                             Toast.LENGTH_SHORT).show();
                                 } catch (ParseException e) {
@@ -124,5 +210,205 @@ public class CreateEventActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         });
+    }
+
+    private void showDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog dialog = new DatePickerDialog(
+                context,
+                android.R.style.ThemeOverlay_Material_Dialog_Alert,
+                mDateSetListener,
+                year, month, day
+        );
+
+        dialog.getDatePicker().setMinDate(new Date().getTime() - 1000);
+        dialog.show();
+    }
+
+    private void showGuestDialog(View v) {
+        LinearLayout container = new LinearLayout(context);
+        container.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(50, 0, 50, 0);
+
+        final EditText guest_email = new EditText(v.getContext());
+        guest_email.setLayoutParams(lp);
+        guest_email.setLayoutParams(lp);
+        guest_email.setGravity(android.view.Gravity.TOP|android.view.Gravity.LEFT);
+        guest_email.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        guest_email.setLines(1);
+        guest_email.setMaxLines(1);
+        container.addView(guest_email, lp);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setTitle(R.string.label_add_guest);
+        builder.setMessage(R.string.label_add_guest_message);
+        builder.setView(container);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // overriding the positive dialog button after show
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = guest_email.getText().toString().trim();
+                if(email.isEmpty()) {
+                    guest_email.setError("Email must be provided.");
+                    guest_email.requestFocus();
+                }
+                else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    guest_email.setError("Please provide valid email.");
+                    guest_email.requestFocus();
+                }
+                else if(guestListArray.contains(email)) {
+                    guest_email.setError("Email already added.");
+                    guest_email.requestFocus();
+                }
+                else {
+                    guestListArray.add(email);
+                    mGuestListView.setText(guestListArray.toString().substring(1, guestListArray.toString().length()-1));
+                    Toast.makeText(context, "Guest added.", Toast.LENGTH_SHORT).show();
+                    guest_email.setText("");
+                    //dialog.dismiss();
+                }
+            }
+        });
+    }
+
+    private boolean validateData() {
+        String title = mTitle.getText().toString().trim();
+        String date = mDate.getText().toString().trim();
+        String location = mLocation.getText().toString().trim();
+        String description = mDescription.getText().toString().trim();
+        String guestList = mGuestListView.getText().toString().trim();
+
+        if (title.isEmpty()) {
+            mTitle.setError("Title is required.");
+            mTitle.requestFocus();
+            return false;
+        }
+        if (date.isEmpty()) {
+            mDate.setError("Date is required.");
+            //mDate.requestFocus();
+            return false;
+        }
+        if (location.isEmpty()) {
+            mLocation.setError("Location is required.");
+            mLocation.requestFocus();
+            return false;
+        }
+        if (description.isEmpty()) {
+            mDescription.setError("Description is required.");
+            mDescription.requestFocus();
+            return false;
+        }
+        if (guestList.isEmpty()) {
+            mGuestListView.setError("Guests are required.");
+            //mGuestListView.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean addEvent(boolean isPublished) throws ParseException {
+        // Owner Details
+        String ownerID = userObject.getUserID();
+        String ownerFirst = userObject.getFirstName();
+        String ownerLast = userObject.getLastName();
+
+        // Event Details
+        String title = mTitle.getText().toString().trim();
+        String dateString = mDate.getText().toString().trim();;
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dateString);
+        String location = mLocation.getText().toString().trim();
+        String description = mDescription.getText().toString().trim();
+
+        // Instantiate New Event Object
+        com.WKNS.gather.databaseModels.Events.Event event = new Event(title, description, ownerID, ownerFirst,
+                ownerLast, date, location, isPublished);
+
+        // String eventID = intent.getStringExtra("eventID");
+        String eventID = "";
+
+        // Add event to events collection, if it doesn't already exist.
+        // Otherwise, update the event document.
+        if (eventID.isEmpty()) { // Initial Event Creation
+            db.collection("events")
+                    .add(event)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("CreateEventSummary",
+                                    "DocumentSnapshot written with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("CreateEventSummary", "Error writing document", e);
+                        }
+                    });
+        }
+        else { // Event Edit
+            db.collection("events").document(eventID)
+                    .set(event)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("CreateEventSummary", "DocumentSnapshot successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("CreateEventSummary", "Error writing document", e);
+                        }
+                    });
+        }
+
+        return true;
+    }
+
+    private boolean deleteEvent() {
+        // Owner + Event Details
+        // String eventID = intent.getStringExtra("eventID");
+        String eventID = "";
+
+        // If event is in database, delete. Do nothing otherwise.
+        if (!eventID.isEmpty()) {
+            db.collection("events").document(eventID)
+                    .delete()
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("CreateEventSummary", "DocumentSnapshot successfully deleted!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("CreateEventSummary", "Error deleting document", e);
+                        }
+                    });
+
+        }
+        return false;
     }
 }

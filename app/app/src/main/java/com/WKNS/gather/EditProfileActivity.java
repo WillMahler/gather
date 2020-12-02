9faf74acde7b48842ea0f8b8460c774d64133f64
Shaun Moini <shaunmoinii@gmail.com>
2020-12-02 16:38:44 -0500

    User profile editing (#92, #84)
    Phone number and bio added to user documents & minor refactoring.
    User profile picture now downloaded only once and stored in user object as bitmap object.
    Profile fragment updated to show phone number and bio as well.
    User profile editing now implemented: users can edit profile image, phone number, and bio (#84).
    Email and phone number in profile fragment now linkified.
    Added check to see if user phone number or bio is empty.
    Any additional characters in phone number will now be removed when registering and editing profile.

---
+++ app/app/src/main/java/com/WKNS/gather/EditProfileActivity.java
@@ -0,0 +1,190 @@
+package com.WKNS.gather;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.WKNS.gather.databaseModels.Users.User;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.gson.Gson;
+
+import java.util.UUID;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    public static final String TAG = EditProfileActivity.class.getSimpleName();
+    private FirebaseFirestore db;
+    private FirebaseStorage storage;
+    private StorageReference mStorageRef;
+    private User userObject;
+    private Uri imageURI;
+
+    private Toolbar actionbar;
+    private ImageView mProfileImage;
+    private TextView mPhoneNum, mBio;
+    private ProgressBar mProgress;
+    private Button mSave;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        // setting title of action bar
+        actionbar = findViewById(R.id.actionbar);
+        setSupportActionBar(actionbar);
+        getSupportActionBar().setTitle("Edit Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_round_arrow_back_ios_24));
+
+        mProfileImage = findViewById(R.id.profileImage);
+        mPhoneNum = findViewById(R.id.editText_phone_number);
+        mBio = findViewById(R.id.editText_bio);
+        mProgress = findViewById(R.id.progressBar);
+        mSave = findViewById(R.id.save_button);
+
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
+
+        Gson gson = new Gson();
+        final String userObjectString = getIntent().getStringExtra("userObjectString");
+        userObject = gson.fromJson(userObjectString, User.class);
+
+        mProfileImage.setImageBitmap(userObject.getProfileBitmap());
+        mPhoneNum.setText(userObject.getPhoneNum());
+        mBio.setText(userObject.getBio());
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNum = mPhoneNum.getText().toString().trim();
+                String bio = mBio.getText().toString().trim();
+
+                if (phoneNum.contains(" ") || phoneNum.contains("-") || phoneNum.contains("(") || phoneNum.contains(")")) {
+                    phoneNum = phoneNum.replaceAll("[()\\s-]+", "");
+                }
+
+                if (imageURI == null && phoneNum.equals(userObject.getPhoneNum()) && bio.equals(userObject.getBio())) {
+                    finish();
+                    return;
+                }
+
+                if (!phoneNum.isEmpty() && !android.util.Patterns.PHONE.matcher(phoneNum).matches()) {
+                    mPhoneNum.setError("Please enter valid phone number.");
+                    mPhoneNum.requestFocus();
+                    return;
+                }
+
+                if (imageURI == null) {
+                    mProgress.setVisibility(View.VISIBLE);
+                    updateUserProfile("", phoneNum, bio);
+                    return;
+                }
+
+                mProgress.setVisibility(View.VISIBLE);
+                uploadPictureToDB(imageURI);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode==RESULT_OK && data != null && data.getData() != null) {
+            imageURI = data.getData();
+            mProfileImage.setImageURI(imageURI);
+        }
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    private void uploadPictureToDB(Uri file) {
+        final String randomKey = UUID.randomUUID().toString();
+        final StorageReference ref = mStorageRef.child("profile_images/" + randomKey);
+        UploadTask uploadTask = ref.putFile(file);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                // Continue with the task to get the download URL
+                return ref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    updateUserProfile(task.getResult().toString(), mPhoneNum.getText().toString().trim(), mBio.getText().toString().trim());
+                }
+                else {
+                    Log.d(TAG, "User profile update failed.");
+                }
+            }
+        });
+    }
+
+    private void updateUserProfile(String photoUrl, String phoneNum, String bio) {
+        DocumentReference documentReference = db.collection("users").document(userObject.getUserID());
+        if(!photoUrl.equals("")) {
+            documentReference.update("profileImg", photoUrl);
+        }
+        documentReference.update("phoneNum", phoneNum);
+        documentReference.update("bio", bio).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d(TAG, "user document update success");
+                Toast.makeText(getApplicationContext(), "Profile updated!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "user document update failed: " + e.toString());
+                Toast.makeText(getApplicationContext(), "User profile update failed: " + e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
--- app/app/src/main/java/com/WKNS/gather/databaseModels/Users/User.java
+++ app/app/src/main/java/com/WKNS/gather/databaseModels/Users/User.java
@@ -1,18 +1,24 @@
 package com.WKNS.gather.databaseModels.Users;
 
+import android.graphics.Bitmap;
+
 // Represents a user in the Users collection
 public class User {
-    private String userID, email, firstName, lastName, profileImg;
+    private String userID, email, phoneNum, firstName, lastName, profileImg, bio;
+    private Bitmap profileBitmap;
 
     // Constructors
     public User() {}
 
-    public User(String userID, String email, String firstName, String lastName, String profileImg) {
+    public User(String userID, String email, String phoneNum, String firstName, String lastName, String profileImg, String bio) {
         this.userID = userID;
         this.email = email;
+        this.phoneNum = phoneNum;
         this.firstName = firstName;
         this.lastName = lastName;
         this.profileImg = profileImg;
+        this.bio = bio;
+        this.profileBitmap = null;
     }
 
     // Getters + Setters
@@ -22,6 +28,9 @@ public class User {
     public String getEmail() { return this.email; }
     public void setEmail(String email) { this.email = email; }
 
+    public String getPhoneNum() { return this.phoneNum; }
+    public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
@@ -30,4 +39,10 @@ public class User {
 
     public String getProfileImage() { return profileImg; }
     public void setProfileImage(String profileImage) { this.profileImg = profileImage; }
+
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
+
+    public Bitmap getProfileBitmap() { return profileBitmap; }
+    public void setProfileBitmap(Bitmap profileBitmap) { this.profileBitmap = profileBitmap; }
 }

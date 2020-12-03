5f4091b58532a7138e65cfb593b6fc512809fddb
Shaun Moini <shaunmoinii@gmail.com>
2020-12-02 20:47:53 -0500

    Updated layout resource file of UserProfileActivity to reflect changes made in the profile fragment.

--- app/app/src/main/java/com/WKNS/gather/UserProfileActivity.java
+++ app/app/src/main/java/com/WKNS/gather/UserProfileActivity.java
@@ -3,30 +3,29 @@ package com.WKNS.gather;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    private ImageView mImageView;
     private Toolbar actionbar;
-    private TextView firstName, lastName, email;
+    private ImageView mProfileImage;
+    private TextView mName, mEmail, mPhoneNum, mBio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        firstName = findViewById(R.id.firstName_external);
-        lastName = findViewById(R.id.lastName_external);
-        email = findViewById(R.id.email_external);
-        mImageView = findViewById(R.id.profileImage);
+        mProfileImage = findViewById(R.id.profileImage);
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mPhoneNum = findViewById(R.id.phoneNum);
+        mBio = findViewById(R.id.bio);
 
         // TODO Remove these later, this is just for display for the D2 demo
-        firstName.setText("Gordon");
-        lastName.setText("Ramsay");
-        mImageView.setImageResource(R.drawable.ic_baseline_person_24);
+        mName.setText("Gordon\nRamsay");
+        mProfileImage.setImageResource(R.drawable.ic_baseline_person_24);
 
         // setting title of action bar
         actionbar = findViewById(R.id.actionbar);

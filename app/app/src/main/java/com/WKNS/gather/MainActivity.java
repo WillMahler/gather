f29f118a7e75ab1214a8c7e7f9362ac98fcd9411
Shaun Moini <shaunmoinii@gmail.com>
2020-12-05 18:03:24 -0500

    Pictures (#99), closes #75
    - Phone number and bio added to user documents & minor refactoring.
    - User profile picture now downloaded only once and stored in user object as bitmap object.
    - Profile fragment updated to show phone number and bio as well.
    - User profile editing now implemented: users can edit profile image, phone number, and bio.
    - Email and phone number in profile fragment now linkified.
    - Added check to see if user phone number or bio is empty.
    - Any additional characters in phone number will now be removed when registering and editing profile.
    - Event images implemented.
    - Search fragment removed from project.
    - Updated layout resource file of UserProfileActivity to reflect changes made in the profile fragment
    - Added padding to bio
    - Set the id of bio textviews in ProfileFragment vs UserProfileActivity differently to avoid conflicts
    - Updated testData classes to match updated db models
    - Added image loading for cards for Users and Events
    - Added image loading for EventDetailsSummary
    - If no image is present, default image will display instead
    - Updated Default Event Page's Image

--- app/app/src/main/java/com/WKNS/gather/MainActivity.java
+++ app/app/src/main/java/com/WKNS/gather/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         mUserEvents = new ArrayList<UserEvent>();
         // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_search, R.id.navigation_create_event, R.id.navigation_notification, R.id.navigation_profile).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_create_event, R.id.navigation_notification, R.id.navigation_profile).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
